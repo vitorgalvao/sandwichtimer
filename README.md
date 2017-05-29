14a1112a01d3653b21d847d4d91bc29a6747a0eb
Vítor Galvão <info@vitorgalvao.com>
2017-05-29 20:17:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+To support my continued open-source work, pick a method:
+
+[<img src='https://upload.wikimedia.org/wikipedia/commons/5/53/PayPal_2014_logo.svg' height='18' alt='Support via Paypal'>](https://www.paypal.me/vitorgalvao)&nbsp;&nbsp;
+[<img src='https://upload.wikimedia.org/wikipedia/commons/c/c5/Bitcoin_logo.svg' height='15' alt='Support via Bitcoin'>](http://vitorgalvao.com/bitcoin_tip_jar.html)&nbsp;&nbsp;
+[<img src='https://dl.dropboxusercontent.com/s/y3pft1fbmer5v22/society6.svg' height='19' alt='Support via Society6'>](https://society6.com/vitorgalvao)
+
+# <img src='https://i.imgur.com/Wi6JyPt.png' width='45' align='center' alt='SandwichTimer logo'> SandwichTimer
+
+Use a CLI to set timers. Built for use with the [SandwichTimer Alfred Workflow](https://github.com/vitorgalvao/alfred-workflows/tree/master/SandwichTimer).
+
+## Usage
+ 
+SandwichTimer was initally built to be a [Pomodoro](https://en.wikipedia.org/wiki/Pomodoro_Technique) timer controllable via CLI, but it can be used as a regular timer. While you can simply open the app and a Pomodoro cycle will start, it’s meant to be called with its CLI:
+
+```bash
+SandwichTimer.app/Contents/MacOS/SandwichTimer "{{time_in_minutes}}"
+```
+
+Unlike most other timers, only the remaining minutes are shown, not the seconds.
+
+<img src="https://i.imgur.com/5BJ2IR1.png" width="51"> <img src="https://i.imgur.com/75pSIEh.png" width="51">
+
+Calling via CLI with no argument or `pomodoro` as the argument will initiate a repeating Pomodoro cycle, with actionable notifications at each stage.
+
+<img src="https://i.imgur.com/l6v9q1c.png" width="378" align="left">
+<img src="https://i.imgur.com/TKOa5E3.png" width="378">
+
+Calling it with a number will initiate a timer. Its notification will continue firing until you quit it.
+
+<img src="https://i.imgur.com/Jrzk411.png" width="378">
+
+Calling it with `quit` as the argument will exit all running timers.
+
+## Install
+
+[Download the latest version](https://github.com/vitorgalvao/sandwichtimer/releases), or install via the [SandwichTimer Alfred Workflow](https://github.com/vitorgalvao/alfred-workflows/tree/master/SandwichTimer).
+
+## Development
+
+Built with [Electron](http://electron.atom.io).
+
+`npm start` will call `electron main.js` and only then give the arguments. This means that while testing, an extra argument is passed on the command line. As such, the `process.argv` array positions need to be increased by one when testing, and be returned to their original state before building the app.
+
+##### Commands
+
+- Install dependencies: `npm install`
+- Run: `npm start`
+- Build for macOS: `npm run build-macos`
+- Build for all platforms: `npm run build`
+
+Currently, only macOS is supported. I do not intend to officially support other platforms in the near future since I cannot consistently and reliably test on them, but am willing to add support if someone wants to collaborate in doing the legwork.
+
+#### License
+
+The Unlicense (Public Domain, essentially)
---
+++ app/main.js
@@ -0,0 +1,152 @@
+'use strict';
+
+const appName = 'SandwichTimer';
+
+// Global references to avoid problems on garbage collection
+let appTray;
+let notification;
+
+const {app, Menu, Notification, Tray} = require('electron');
+const exec = require('child_process').exec;
+const menuTemplate = [
+  { label: 'Start Pomodoro', click: function () { runPomodoro('work'); } },
+  { label: 'Stop Timer', click: function () { stopTimer(); } },
+  { type: 'separator' },
+  { label: 'Quit ' + appName, click: function () { app.quit(); } }
+]
+const trayMenu = Menu.buildFromTemplate(menuTemplate);
+
+let globalCountdownId = Math.random(); // By setting a global ID each timer is checked against, we ensure they are terminated correctly. This way we can have a high setTimeout without worrying about overlapping timers.
+
+function asyncWait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function msToMin(milliseconds) {
+  return milliseconds / 1000 / 60;
+}
+
+function minToMs(minutes) {
+  return minutes * 1000 * 60;
+}
+
+function showMenuOption(makeVisible) {
+  if (makeVisible === 'Start') {
+    trayMenu.items[0].visible = true;
+    trayMenu.items[1].visible = false;
+    return;
+  }
+
+  if (makeVisible === 'Stop') {
+    trayMenu.items[0].visible = false;
+    trayMenu.items[1].visible = true;
+    return;
+  }
+}
+
+async function showNotification(time) {
+  let options;
+
+  if (time === 'work') {
+    options = {
+      title: 'Finished work time',
+      body: 'Start the break at any time',
+      sound: 'Blow',
+      actions: [{ type: 'button', text: 'Start break' }]
+    }
+  } else if (time === 'break') {
+    options = {
+      title: 'Break is over',
+      body: 'Ready to start a new pomodoro whenever you want',
+      sound: 'Blow',
+      actions: [{ type: 'button', text: 'New pomodoro' }]
+    }
+  } else {
+    const plurality = time == '1' ? 'minute' : 'minutes'
+
+    options = {
+      title: 'Timer done!',
+      body: 'It was set for ' + time + ' ' + plurality,
+      sound: 'Submarine',
+      actions: [{ type: 'button', text: 'Quit' }]
+    }
+  }
+
+  notification = new Notification(options);
+  notification.show();
+
+  if (time === 'work') {
+    appTray.setTitle('Work over');
+    notification.on('action', function() { runPomodoro('break'); });
+    notification.on('click',  function() { runPomodoro('break'); });
+  } else if (time === 'break') {
+    appTray.setTitle('Break over');
+    notification.on('action', function() { runPomodoro('work'); });
+    notification.on('click',  function() { runPomodoro('work'); });
+  } else {
+    appTray.setTitle('Timer over');
+    notification.on('action', function() { app.quit(); });
+    notification.on('click',  function() { app.quit(); });
+  }
+}
+
+function stopTimer() {
+  globalCountdownId = Math.random();
+  showMenuOption('Start');
+  appTray.setTitle('');
+}
+
+async function runPomodoro(time) {
+  let minutes = (time === 'work') ? '25' : '5'
+  if (await countdown(minutes)) showNotification(time); // Only show notification if timer ended successfully
+}
+
+async function countdown(minutesToWait) {
+  showMenuOption('Stop');
+  let localCountdownId = globalCountdownId;
+
+  const duration = minToMs(minutesToWait);
+  const start = Date.now();
+  let remainingMinutes = msToMin(duration);
+
+  while (remainingMinutes > 0) {
+    appTray.setTitle(remainingMinutes.toString());
+    await asyncWait(20000);
+
+    if (localCountdownId !== globalCountdownId) return false; // This means the timer was terminated manually (and the global ID was changed)
+
+    remainingMinutes = Math.ceil(msToMin(duration - (Date.now() - start)));
+  }
+
+  stopTimer();
+  return true;
+}
+
+app.on('ready', function(){
+  // For testing, increase array position by one
+  const argument = process.argv[1];
+
+  if (argument === 'quit') {
+    exec('pkill -i ' + appName);
+    app.quit();
+    return; // if we do not return, the next lines will still execute before the app quits and we'll get a flash of a new instance
+  }
+
+  appTray = new Tray(__dirname + '/iconTemplate@2x.png');
+  appTray.setContextMenu(trayMenu);
+
+  if ((isNaN(argument)) || argument === '' || argument === 0 || argument === 'pomodoro') {
+    runPomodoro('work');
+    return;
+  }
+
+  (async function() {
+    await countdown(argument);
+
+    // For a regular timer, keep making noise until the notification is acted upon
+    while (true) {
+      showNotification(argument);
+      await asyncWait(2000);
+    }
+  })();
+});
